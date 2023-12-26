4eec57582e66cde4cb708a07872b4ea32381b462
Alejandro Front <desarrollo@qsocialnow.com>
2023-12-26 11:43:44 -0300

    user y pass

--- src/componentes/Formularios/Login/index.jsx
+++ src/componentes/Formularios/Login/index.jsx
@@ -26,7 +26,7 @@ const FormLogin = () => {
    
       dispatch(loginSuccess(user));
       setRedirectToDashboard(true); // Establece el estado para redireccionar a "/dashboard"
-    } else if (username === 'qsnvzla' && password === 'qsnvzla2023') {
+    } else if (username === 'argmov' && password === 'argmov2023') {
       const user = {
         username: 'qsnvzla',
         roles: ['user'],
